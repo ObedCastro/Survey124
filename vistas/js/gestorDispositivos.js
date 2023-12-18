21ffe947885e1c0b2d75a8dcd7f95b48c29358d1
Obed Castro <37159837+ObedCastro@users.noreply.github.com>
2023-12-18 16:24:37 -0600

    Add files via upload

---
+++ vistas/js/gestorDispositivos.js
@@ -0,0 +1,34 @@
+
+//Cargar la tabla de dispositivos de manera dinámica
+/*$.ajax({
+    url: "ajax/tablaDispositivos.ajax.php",
+    success: function(respuesta){
+        console.log("Respuesta", respuesta);
+    }
+});*/
+
+
+//Traducir datatable
+$('#datatable').DataTable({
+    "ajax": "ajax/tablaDispositivos.ajax.php",
+    language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
+});
