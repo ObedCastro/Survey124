8708d99dccc8afd5d23a881efb9e2310e3e2d193
Obed Castro <37159837+ObedCastro@users.noreply.github.com>
2023-12-18 16:23:06 -0600

    Add files via upload

--- modelos/dispositivos.modelo.php
+++ modelos/dispositivos.modelo.php
@@ -4,12 +4,26 @@ require_once "conexion.php";
 
 class ModeloDispositivos{
 
-    static public function mdlMostrarDispositivos($tabla){
-        $sql = "SELECT * FROM $tabla";
-        $stmt = Conexion::conectar()->prepare($sql);
-        $stmt->execute();
+    static public function mdlMostrarDispositivos($tabla, $item, $valor){
+        
+        if($item != null){
+            $sql = "SELECT * FROM $tabla WHERE $item = :$item";;
+            $stmt = Conexion::conectar()->prepare($sql);
+            $stmt->bindParam(":".$item, $valor, PDO::PARAM_STR);
+            $stmt->execute();
+            
+            return $stmt->fetch();
+            
+        } else{
+            $sql = "SELECT * FROM $tabla ORDER BY iddispositivo DESC";
+            $stmt = Conexion::conectar()->prepare($sql);
+            $stmt->execute();
+            
+            return $stmt->fetchAll();
+    
+        }
 
-        return $stmt->fetchAll();
+        $stmt->close();
     }
 
     
