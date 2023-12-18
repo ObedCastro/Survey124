f64163f83e8c1b01487d08d20fc8cedd0d82df31
Obed Castro <37159837+ObedCastro@users.noreply.github.com>
2023-12-18 16:23:22 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 //CONTROLADORES
 require_once "controladores/base.controlador.php";
 require_once "controladores/administradores.controlador.php";
+require_once "controladores/inicio.controlador.php";
+require_once "controladores/dispositivos.controlador.php";
 
 //MODELOS
 require_once "modelos/administradores.modelo.php";
+require_once "modelos/inicio.modelo.php";
+require_once "modelos/dispositivos.modelo.php";
 
 $base = new ControladorBase();
 $base->base();
